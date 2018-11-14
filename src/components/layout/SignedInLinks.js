4ce7aab12ce11cd241d4ce0cd39b7890b7093898
Kiran Villa <kiranvilla1234@gmail.com>
2018-11-14 23:34:57 +0530

    modified

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -12,7 +12,7 @@ const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to="/create">New Project</NavLink></li>
-            <li><a onClick={props.signOut}>Log Out</a></li>
+            <li><NavLink to="/" onClick={props.signOut}>Log Out</NavLink></li>
             <li><NavLink to={"/userProjects/" + uid} className="btn btn-floating pink lighten-1">{initial}</NavLink></li>
         </ul>
     )
--- src/components/projects/userProjects.js
+++ src/components/projects/userProjects.js
@@ -7,25 +7,28 @@ const userProjects = (props) => {
 
     const userId = props.match.params.id;
     const userProjects = props.userProjects ? props.userProjects.filter(i => i.authorId === userId) : null
-    // console.log(userProjects)
+    console.log(userProjects)
 
     if (props.userProjects) {
         return (
             <div>
                 <h4>Your Projects</h4>
-
-                {userProjects.map(project => {
-                    return (
-                        <div key={project.id} className="card z-depth-0 project-summary">
-                            <div className="card-content grey-text text-darken-3 ">
-                                <span className="card-title">{project.title}</span>
-                                <p>{project.content}</p>
-                                <p>{project.authorFirstName} {project.authorLastName}</p>
-                                <p className="grey-text">10th, November 7pm</p>
+                {   
+                    userProjects.length !== 0 ? userProjects.map(project => {
+                        return (
+                            <div key={project.id} className="card z-depth-0 project-summary">
+                                <div className="card-content grey-text text-darken-3 ">
+                                    <span className="card-title">{project.title}</span>
+                                    <p>{project.content}</p>
+                                    <p>{project.authorFirstName} {project.authorLastName}</p>
+                                    <p className="grey-text">10th, November 7pm</p>
+                                </div>
                             </div>
-                        </div>
+                        )
+                    }):(
+                        <div>No Projects</div>
                     )
-                })}
+                }
             </div>
         )
     } else {
